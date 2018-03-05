7e6e6661d64d82a1f368a59f010724b868b8c52c
Milan Nikolic <gen2brain@gmail.com>
2018-03-05 18:17:10 +0100

    Test flush

--- encode_test.go
+++ encode_test.go
@@ -56,3 +56,49 @@ func TestEncode(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestEncodeFlush(t *testing.T) {
+	buf := bytes.NewBuffer(make([]byte, 0))
+
+	opts := &Options{
+		Width:     640,
+		Height:    480,
+		FrameRate: 25,
+		Tune:      "zerolatency",
+		Preset:    "fast",
+		Profile:   "high",
+		LogLevel:  LogDebug,
+	}
+
+	enc, err := NewEncoder(buf, opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	img := NewYCbCr(image.Rect(0, 0, opts.Width, opts.Height))
+	draw.Draw(img, img.Bounds(), image.Black, image.ZP, draw.Src)
+
+	for i := 0; i < opts.Width/2; i++ {
+		img.Set(i, opts.Height/2, color.RGBA{255, 0, 0, 255})
+
+		err = enc.Encode(img)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	err = enc.Flush()
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = enc.Close()
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = ioutil.WriteFile(filepath.Join(os.TempDir(), "test.high.264"), buf.Bytes(), 0644)
+	if err != nil {
+		t.Error(err)
+	}
+}
