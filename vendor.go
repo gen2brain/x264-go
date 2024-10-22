732e1bdb7da2abf9709e6ec1a4ab3f8df23021d6
Milan Nikolic <gen2brain@gmail.com>
2024-10-22 20:20:00 +0200

    Fix vendor

--- vendor.go
+++ vendor.go
@@ -6,5 +6,11 @@ import (
 	_ "github.com/gen2brain/x264-go/x264c/external"
 	_ "github.com/gen2brain/x264-go/x264c/external/x264"
 	_ "github.com/gen2brain/x264-go/x264c/external/x264/common"
+	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/aarch64"
+	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/arm"
+	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/mips"
+	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/opencl"
+	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/ppc"
+	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/x86"
 	_ "github.com/gen2brain/x264-go/x264c/external/x264/encoder"
 )
