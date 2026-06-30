b55492686b2234b3f5c904f1f145878b670b92ac
Milan Nikolic <gen2brain@gmail.com>
2026-06-30 21:25:47 +0200

    Update vendor

--- vendor.go
+++ vendor.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/gen2brain/x264-go/x264c/external/x264/common"
 	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/aarch64"
 	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/arm"
+	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/loongarch"
 	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/mips"
 	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/opencl"
 	_ "github.com/gen2brain/x264-go/x264c/external/x264/common/ppc"
